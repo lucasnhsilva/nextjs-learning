e326e0c713d29dafd3b4550a3634d3d57e3bc563
lucasnathandev <lucas.develop.prog@gmail.com>
2025-07-07 22:30:28 -0300

    feat: start with server components and fetching data directly

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,29 @@
-export default function Posts() {
+interface ResponseProps {
+  posts: PostProps[];
+}
+
+interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+export default async function Posts() {
+  const response = await fetch("https://dummyjson.com/posts");
+  const data: ResponseProps = await response.json();
+
   return (
     <div>
-      <h1>Posts</h1>
+      <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Posts</h1>
+      <ul className="flex flex-col gap-4 mx-2">
+        {data.posts.map((post) => (
+          <li key={post.id} className="bg-gray-700 p-4 rounded-md ">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
